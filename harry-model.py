1e65985d6d8b27508ddb2fd36b5aa2a863449bee
nahuel salto <mnahuels01@gmail.com>
2026-04-22 00:51:05 -0300

    Chats_prompttemplate a Openai(con Api key), y ollama

--- harry-model.py
+++ harry-model.py
@@ -2,6 +2,5 @@ from langchain_ollama import OllamaLLM
 
 llm = OllamaLLM(model="harry-model")
 respuesta = llm.invoke("Hola, ¿cómo estás?")
-respuesta = llm.invoke("Si no tienen sentimiento como pueden responder a preguntas como esta?")
 respuesta = llm.invoke("Quien descubrio america ?")
 print(respuesta)
